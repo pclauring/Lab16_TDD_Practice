ecb2395a8103c769238a9581a07d40d310f068ca
Pierce C Lauring <pclauring@gmail.com>
2017-10-31 16:26:25 -0400

    first commit

---
+++ Lab16_Find_Primes/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab16_Find_Primes
+{
+    class Class1
+    {
+        // public class HikerTest
+        // {
+        //     [Test]
+        //     public void Case1()
+        //     {
+        //         Assert.AreEqual(2, Hiker.Answer(1));
+        //     }
+        //     [Test]
+        //     public void Case2()
+        //     {
+        //         Assert.AreEqual(3, Hiker.Answer(2));
+        //     }
+        //     [Test]
+        //     public void Case3()
+        //     {
+        //         Assert.AreEqual(5, Hiker.Answer(3));
+        //     }
+        //     [Test]
+        //     public void Case4()
+        //     {
+        //         Assert.AreEqual(29, Hiker.Answer(10));
+        //     }
+        // }
+    }
+}
--- Lab16_Find_Primes/Program.cs
+++ Lab16_Find_Primes/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab16_Find_Primes
 {
@@ -10,6 +6,79 @@ namespace Lab16_Find_Primes
     {
         static void Main(string[] args)
         {
+            bool repeat;
+            int[] primeNums = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+
+            Console.WriteLine("Let's Locate Some Primes!\n");
+            Console.WriteLine("This application will find you any prime, in order, from first prime on.");
+
+            // int num = 100;
+            //
+            //     bool isPrime = true;
+            //     for (int i = 0; i <= num; i++)
+            //     {
+            //         for (int j = 2; j <= num; j++)
+            //         {
+            //             if (i != j && i % j == 0)
+            //             {
+            //                 isPrime = false;
+            //                 break;
+            //             }
+            //         }
+            //         if (isPrime)
+            //         {
+            //             Console.WriteLine(i);
+            //         }
+            //         isPrime = true;
+            // }
+
+            do
+            {
+                Console.Write("Which prime number are you looking for?");
+
+                int primeSequence = -1;
+                bool success = false;
+                while (!success)
+                {
+                    success = int.TryParse(Console.ReadLine(), out primeSequence);
+
+                    if (primeSequence < 1 || primeSequence > 12)
+                    {
+                        success = false;
+                    }
+                }
+
+                Console.WriteLine($"The number {primeSequence} is {primeNums[primeSequence - 1]}\n");
+                Console.WriteLine("Do you want to find a new prime number?(y/n)\n");
+                repeat = GetYesorNo();
+            } while (repeat);
+
+        }
+        private static bool GetYesorNo()
+        {
+            bool valid = true;
+            bool repeat = true;
+            while (valid)
+            {
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "y" || answer == "yes")
+                {
+                    valid = false;
+                    repeat = true;
+                }
+                else if (answer == "n" || answer == "no")
+
+                {
+                    valid = false;
+                    repeat = false;
+                }
+                else
+                {
+                    Console.Write("Did not enter a valid input. Please enter (y/n): ");
+                }
+            }
+
+            return repeat;
         }
     }
 }
